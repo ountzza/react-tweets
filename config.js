b2299daa32ec27b7effd687da62621065ab10432
ountzza <pond1982a@gmail.com>
2014-12-17 15:01:57 +0700

    add key

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'put-yours-here',
-    consumer_secret: 'put-yours-here',
-    access_token_key: 'put-yours-here',
-    access_token_secret: 'put-yours-here'
+    consumer_key: 'lDNh0RhBE0SNKTAyXW1fBM9MD',
+    consumer_secret: 'hOdkt8vxDiG5Gp0TaapOXRkuRz5vbysD7iaXH3LdkCrzoD7mHf',
+    access_token_key: '29405689-J3liaBSgOdH8aqHrMisQyphNYrisYZ8Ua5nIpSlux',
+    access_token_secret: 'nXlEyC96hOdSQ40dM4heR8ytFABXzYhOUZhUoJNJ7N9Lx'
   }
 }
